bad92d61877462edb932877607a24a7eb23c0c8f
NAX CHAUDHARY <chaudharynax27@gamil.com>
2026-06-14 16:20:28 +0530

    Day 2: Created Jarvis core architecture

---
+++ core/assistant.py
@@ -0,0 +1,6 @@
+class Jarvis:
+    def __init__(self):
+        print("Jarvis Initialized Successfully")
+
+    def start(self):
+        print("Jarvis is Ready")
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello Sir, Jarvis Online")
+from core.assistant import Jarvis
+
+def main():
+    print("Starting Jarvis...")
+
+    assistant = Jarvis()
+    assistant.start()
+
+if __name__ == "__main__":
+    main()
